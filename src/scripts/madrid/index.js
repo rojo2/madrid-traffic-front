b8867ab0726f84f2c8458a5a25326d576aba473a
Aitor <aitor@rojo2.com>
2016-12-19 16:05:38 +0100

    Adds some app code

--- src/scripts/madrid/index.js
+++ src/scripts/madrid/index.js
@@ -1,7 +1,35 @@
 import Inferno from "inferno";
 import Component from "inferno-component";
 
+function Application(props) {
+  return (
+    <div>
+      Hello World
+      <Map />
+      <Detail />
+    </div>
+  );
+}
+
+function Map(props) {
+  return (
+    <div>Map</div>
+  );
+}
+
+function Detail(props) {
+  return (
+    <div>Detail</div>
+  );
+}
+
+function Timeline(props) {
+  return (
+    <div>Timeline</div>
+  );
+}
+
 Inferno.render(
-  <div>Hello World</div>,
+  <Application />,
   document.getElementById("Application")
 );
