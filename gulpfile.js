4dcf4b2b49fb813bc22d4f5a3971aa196b2a1a66
Omar <mekanoide@gmail.com>
2016-12-19 15:07:58 +0100

    gulpfile fixed

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const browserify = require("browserify");
 const babelify = require("babelify");
 const watchify = require("watchify");
 const exorcist = require("exorcist");
-const inferno = require("babel-preset-inferno");
+const inferno = require("babel-plugin-inferno");
 const bs = require("browser-sync");
 const chaf = require("connect-history-api-fallback");
 const autoprefixer = require("autoprefixer");
@@ -16,22 +16,24 @@ const cssnext = require("postcss-cssnext");
  * Devuelve si estamos en producción o no.
  */
 function isProduction() {
-	return process.env.NODE_ENV === "production";
+  return process.env.NODE_ENV === "production";
 }
 
 /**
  * Crea el bundler  (browserify + watchify)
  */
 function createBundler() {
-	const bundler = browserify({
-		debug: !isProduction(),
-		paths: ["src/scripts"]
-	}).plugin(watchify)
+  const bundler = browserify({
+      debug: !isProduction(),
+      paths: ["src/scripts"]
+    }).plugin(watchify)
     .plugin(inferno)
-		.transform(babelify, { presets: ["latest"] })
+    .transform(babelify, {
+      presets: ["latest"]
+    })
     .add("src/scripts/madrid/index.js");
 
-	return bundler;
+  return bundler;
 }
 
 // Bundler
@@ -41,57 +43,61 @@ let bundler = null;
  * Compila el código javascript.
  */
 function bundle() {
-	if (!bundler) {
-		bundler = createBundler();
-	}
-	const stream = bundler
-		.bundle()
-		.on("error", (err) => {
-			plugins.util.log(err.message);
-			bs.notify("browserify error!");
-		})
-		.pipe(exorcist("index.js.map"))
-		.pipe(source("index.js"))
-		.pipe(gulp.dest("dist"));
-	if (bs.active) {
-		stream.pipe(bs.stream({ once: true }));
-	}
-	return stream;
+  if (!bundler) {
+    bundler = createBundler();
+  }
+  const stream = bundler
+    .bundle()
+    .on("error", (err) => {
+      plugins.util.log(err.message);
+      bs.notify("browserify error!");
+    })
+    .pipe(exorcist("index.js.map"))
+    .pipe(source("index.js"))
+    .pipe(gulp.dest("dist"));
+  if (bs.active) {
+    stream.pipe(bs.stream({
+      once: true
+    }));
+  }
+  return stream;
 }
 
 gulp.task("scripts", () => {
-	return bundle();
+  return bundle();
 });
 
 gulp.task("styles", () => {
-	const processors = [
-		autoprefixer({browsers: ['last 1 version']}),
-		precss(),
-		cssnext(),
-		stylelint()
-	];
-	const stream = gulp.src("src/styles/index.css")
-		.pipe(plugins.plumber());
-		.pipe(postcss(processors))
-		.pipe(gulp.dest("dist"));
-	if (bs.active) {
-		stream.pipe(bs.stream());
-	}
+  const processors = [
+    autoprefixer({
+      browsers: ['last 1 version']
+    }),
+    precss(),
+    cssnext(),
+    stylelint()
+  ];
+  const stream = gulp.src("src/styles/index.css")
+    .pipe(plugins.plumber())
+    .pipe(postcss(processors))
+    .pipe(gulp.dest("dist"));
+  if (bs.active) {
+    stream.pipe(bs.stream());
+  }
 });
 
-gulp.task("build", ["scripts","styles"]);
+gulp.task("build", ["scripts", "styles"]);
 
 gulp.task("watch", ["build"], () => {
-	gulp.watch("src/styles/**/*.css",["styles"]);
+  gulp.watch("src/styles/**/*.css", ["styles"]);
 });
 
 gulp.task("bs", ["watch"], () => {
-	bs.init({
-		server: {
-			baseDir: "dist",
-			middleware: [chaf()]
-		}
-	});
+  bs.init({
+    server: {
+      baseDir: "dist",
+      middleware: [chaf()]
+    }
+  });
 });
 
 gulp.task("default", ["bs"]);
